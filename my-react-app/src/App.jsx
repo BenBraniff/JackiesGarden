871b7086448a00610729ba7097752b535a082885
Benjamin Braniff <Ben.Braniff37@gmail.com>
2026-01-27 23:52:50 -0500

    Working 3d Model !

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+// import stuff for 3d render
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Garden from "../public/garden/Garden";
+import Earth from "../public/earth/Earth";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Jackie's Garden</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Hello Jackie's Garden</h1>
+      <Canvas camera={{ position: [20, 10, 20], fov: 45 }}>
+        <ambientLight />
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <Garden />
+        </Suspense>
+      </Canvas>
     </>
-  ) 
+  );
 }
 
-export default App
+export default App;
